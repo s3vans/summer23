2672a4420b8eb1fbb640c7828da07e22fc9a8f1a
Shayne Evans <shayneevans@gmail.com>
2023-06-19 14:58:33 -0400

    Add basic definition of a level, defender, and attacker.
    Display the level background.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,58 @@
-function preload() { 
+
+class Defender {
+  constuctor(name, img, xp_cost, hp) {
+    this.name = name;
+    this.img = img;
+    this.xp = xp_cost;
+    this.hp = hp;
+  }
 }
 
+class Attacker {
+  constuctor(name, img, hp) {
+    this.name = name;
+    this.img = img;
+    this.hp = hp;
+  }
+}
+
+class Level {
+  constructor(img, defenders, attackers, attack_delay_secs) {
+    this.img = img;
+    this.defenders = defenders;
+    this.attackers = attackers;
+    this.attack_delay_secs = attack_delay_secs;
+  }
+}
+
+let level1_img;
+let pikachu_img;
+let evee_img;
+
+function preload() {
+  level1_img = loadImage('assets/level1.png');
+  pikachu_img = loadImage('assets/pikachu.png');
+  evee_img = loadImage('assets/evee.png');
+}
+
+let level1;
+let pikachu;
+let evee;
+
+const XRESOLUTION = 800;
+const YRESOLUTION = 600;
+
 function setup() {
+  pikachu = new Defender("Pikachu", pikachu_img, /*xp_cost=*/150, /*hp=*/200);
+  evee = new Attacker("Evee", evee_img, /*hp=*/150);
+  level1 = new Level(level1_img, [pikachu], [], 5);
+
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(0);
+  image(level1_img, 0, 0);
 }
 
 /*function windowResized() {
