0dca620dd003cfe4f860755c7957e212fc4d398b
Shayne Evans <shayneevans@gmail.com>
2023-06-19 10:55:54 -0400

    Remove most code to start fresh.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,10 @@
-let screen1;
-let engine;
-
 function preload() { 
-  bg1 = loadImage('assets/bg1.jpg');
 }
 
 function setup() {
-  engine = new GameEngine(windowWidth, windowHeight);
-  screen1 = new Screen(bg1);
-  screen1.show();
-  engine.addScreen(screen1);
-  setInterval(() => screen1.hide(), 5000);
 }
 
 function draw() {
-  engine.update();
-  engine.draw();
-  circle(mouseX, mouseY, 5+mouseX);
 }
 
 /*function windowResized() {
