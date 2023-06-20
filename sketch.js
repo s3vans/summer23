aeac4253ad5ba4d6c8ce24516511d729a45057c7
Shayne Evans <shayneevans@gmail.com>
2023-06-19 22:39:29 -0400

    Restructure assets directory for better oganization.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let levels = [{
     "uid": "level1",
     "name": "Level 1",
-    "img": "assets/level1.png",
+    "img": "assets/pokemon/levels/level1.png",
     "defenderConfigs": [{
         "uid": "pikachu",
         "name": "Pikachu",
-        "img": "assets/pikachu.png",
+        "img": "assets/pokemon/defenders/pikachu.png",
         "xp_cost": 150,
         "hp": 200,
     }],
     "attackerConfigs": [{
         "uid": "evee",
         "name": "Evee",
-        "img": "assets/evee.png",
+        "img": "assets/pokemon/attackers/evee.png",
         "hp": 150,
     }],
   }];
