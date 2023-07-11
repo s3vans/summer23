b02f0efd66d06556969ed5e1b3011efac29fecb0
Shayne Evans <shayneevans@gmail.com>
2023-07-11 13:19:14 -0400

    Implement non-looping behavior in Animatino class.

--- animation.js
+++ animation.js
@@ -52,9 +52,13 @@ class Animation {
     this.lastFrameTime = null;
     this.msPerFrame = Math.floor(1000 / this.fps);
     this.currentFrameNum = 0;
+    this.isDone = false;
   }
 
   update(deltaTime) {
+    if (this.isDone) {
+      return;
+    }
     let nowMs = Date.now();
     if (this.numFrames === null) {
       this.numFrames = Math.floor(this.img.height / this.frameHeight);
@@ -65,6 +69,10 @@ class Animation {
     let timeSinceLastFrame = nowMs - this.lastFrameTime;
     let numFramesToAdvance = Math.floor(timeSinceLastFrame / this.msPerFrame);
     let nextFrame = (this.currentFrameNum + numFramesToAdvance) % this.numFrames;
+    if ((!this.isLooping) && (nextFrame < this.currentFrameNum)) {
+      nextFrame = this.numFrames - 1;
+      this.isDone = true;
+    }
     this.currentFrameNum =  nextFrame;
     if (numFramesToAdvance > 0) {
       this.lastFrameTime = nowMs;
